cbe070d0486a5b0753c90859a95ce5b32ffb6a16
L-F-FF-23 <te23lucflo@falufri.se>
2026-04-01 17:31:10 +0200

    Comments (2026-04-01)
    some stuff I wrote down to help both myself and others reading this code

--- app/src/main/java/com/example/diceshaker/MainActivity.kt
+++ app/src/main/java/com/example/diceshaker/MainActivity.kt
@@ -21,7 +21,9 @@ import androidx.constraintlayout.helper.widget.Grid
 class MainActivity : AppCompatActivity() {
     private lateinit var container: GridLayout
     private lateinit var sensorManager: SensorManager
-    private lateinit var containerList: MutableList<Pair<Int,TextView>>
+    private lateinit var containerList: MutableList<Pair<Int,TextView>> //This is like a dict, I use this and update the gridlayout after it
+
+    //Both container and containerList is here due to the program needing to access both multiple times throughout the code, could be done without them here but then I'm just needlessly fetching them by id over and over again
     private var accelerometer: Sensor? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,6 +40,7 @@ class MainActivity : AppCompatActivity() {
 
         containerList = mutableListOf<Pair<Int,TextView>>()
         container = findViewById<GridLayout>(R.id.dice_container)
+
         val addDiceButton: Button = findViewById<Button>(R.id.add_dice_button)
         val sidesinput: EditText = findViewById<EditText>(R.id.dicesidesinput)
         val resetButton: Button = findViewById<Button>(R.id.reset_Button)
@@ -52,7 +55,7 @@ class MainActivity : AppCompatActivity() {
             containerList.removeAll(containerList)
             updateContainerBox()
         }
-        sensorManager.registerListener(sensorListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(sensorListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL) //The actual thing that checks the sensor
     }
 
     private val sensorListener = object : SensorEventListener {
@@ -63,7 +66,7 @@ class MainActivity : AppCompatActivity() {
             val y = event.values[1]
             val z = event.values[2]
 
-            val acceleration = sqrt((x * x + y * y + z * z).toDouble())
+            val acceleration = sqrt((x * x + y * y + z * z).toDouble()) //x,y,z are all different directions the phone can sense then you do physics math here
 
             if (acceleration > 15) {
                 rollDice()
@@ -71,7 +74,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        }
+        } //this needs to be here to avoid errors but I don't have to use it
     }
 
     fun rollDice(){
@@ -85,15 +88,14 @@ class MainActivity : AppCompatActivity() {
 
         val newDice = TextView(this)
         val gridLayoutParams = GridLayout.LayoutParams().apply {
-            width = 0
+            width = 0 //tells it to use the columSpec weight, I think
             height = dpToPx(80)
-            columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f)
+            columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f) //these would be use to specify where in row and column the object should be, but by using weight I can make it become dynamic changing postion to fit better
             rowSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f)
-        }
+        } //this is so I can tell the new textviews how they should form themselves in the grid
         newDice.layoutParams = gridLayoutParams
         newDice.text = sidesamount.toString()
-        newDice.setAutoSizeTextTypeWithDefaults(TextView.AUTO_SIZE_TEXT_TYPE_UNIFORM)
-        newDice.setTypeface(MONOSPACE)
+        newDice.setTypeface(MONOSPACE) //all digits take up the same space
         newDice.setPadding(20, 5, 20, 5)
 
         containerList.add(Pair(sidesamount,newDice))
@@ -105,7 +107,7 @@ class MainActivity : AppCompatActivity() {
         var count = 0
         container.removeAllViews()
         for (dice in containerList) {
-            dice.second.setAutoSizeTextTypeWithDefaults(TextView.AUTO_SIZE_TEXT_TYPE_UNIFORM)
+            dice.second.setAutoSizeTextTypeWithDefaults(TextView.AUTO_SIZE_TEXT_TYPE_UNIFORM) //changes the font size to fit in the TextView size, had to bump up the minimum api by 2 version, the alternative game me a headache
             count += dice.second.text.toString().toInt()
             container.addView(dice.second)
         }
@@ -114,5 +116,5 @@ class MainActivity : AppCompatActivity() {
 
     fun dpToPx(dp: Int): Int {
         return (dp * resources.displayMetrics.density).toInt()
-    }
+    } //this is pretty simple and neat, allows you to use dp programmatically when adding or modifying objects, will use again in the future
 }
